be8e70f74eba9d565dfb9f7235065159a7bf250d
Sam Blazes <sblazes16@gmail.com>
2017-07-22 20:37:36 -0700

    added a basic SearchInfo struct that contains common search information

--- search/src/search.rs
+++ search/src/search.rs
@@ -1,12 +1,39 @@
 use bitboard::Board;
 use bitboard::Move;
 
+use transposition::TranspositionTable;
+
 use heuristic::Heuristic;
 
 use std::time::Instant;
+use rand::Rng;
 
 pub trait Search<H : Heuristic> {
     fn search(&mut self, board : Board, ms_left : u64, start : Instant) -> Move;
 
     fn set_heuristic(&mut self, hr : Box<H>);
+}
+
+pub struct SearchInfo {
+    pub tt  : TranspositionTable,
+    pub to  : bool,
+    pub st  : Instant,
+    pub sr  : u64, 
+    pub rn  : ThreadRng,
+}
+
+impl SearchInfo {
+    pub fn new() -> SearchInfo {
+        SearchInfo {
+            tt: TranspositionTable::new(20_000_000),
+            to: false,
+            st: Instant::now(),
+            sr: 0,
+            rn: rand::thread_rng,
+        }
+    }
+
+    pub fn set_start(&mut self) {
+        self.st = Instant::now();
+    }
 }
