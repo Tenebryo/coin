7b221859821908e82c51aca177bd987d61ef97d0
Sam Blazes <sblazes16@gmail.com>
2017-07-24 02:42:18 -0700

    PVS test

--- search/src/lib.rs
+++ search/src/lib.rs
@@ -26,6 +26,7 @@ mod tests {
     use bitboard::empty_movelist;
 
     use ::mtdf_id_timeout;
+    use ::pvs_id;
     use ::TranspositionTable;
     use std::time::Instant;
 
@@ -38,13 +39,37 @@ mod tests {
     fn negamax_mtdf_consistency() {
         let mut b = Board::new();
 
-        let m = mtdf_id_timeout(b, Box::new(BasicHeuristic::new()), 34, 10000);
+        //let m = mtdf_id_timeout(b, Box::new(BasicHeuristic::new()), 34, 10000);
 
-        println!("Move: {}", m);
+        //println!("Move: {}", m);
     }
 
     #[test]
     fn monte_carlo_test() {
 
+    }
+
+    #[test]
+    fn pvs_test() {
+        use heuristic::HandmadeHeuristic;
+        use heuristic::ScaledBasicHeuristic;
+
+        let tmp = Box::new(ScaledBasicHeuristic::new(10));
+        let mut tmp_hr = [
+            tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone(), 
+            tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone(), 
+            tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone(), 
+            tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone(), tmp.clone()
+        ];
+
+        let b1 = Board::from_string(
+            // Vec::from("BWWWWBBB\nBWBBWBBB\nBBBWWB B\nBWBBWB  \nBWBBWB  \nBWWWWBBB\nBWWWWBBB\nBBBBBBB \n")
+            Vec::from(" WWWWWW \nWWWBWW W\nWWWWBWWW\nWWWWBWWW\nWWWWWWWW\nWWWWBWWW\n BBBWW W\n WWWWW  \n")
+        );
+
+
+        let mut out_move = pvs_id(b1, &mut tmp_hr, &mut ScaledBasicHeuristic::new(10_000), 9, 1000);
+
+        println!("BEST MOVE: {} FOR \n{}", out_move, b1);
     }
 }
